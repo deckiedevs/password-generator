af2a2a9fb0259cd35f5c82153bd8ba534e35234c
Mila Decker <milacdecker@gmail.com>
2020-10-27 17:02:41 -0400

    added password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
+var generatePassword = function() {
+  
+  // setting character length
+  var charLength = window.prompt('How long would you like your password to be?  Please enter a number between "8" and "128".');
 
+  if (charLength < 8 || charLength > 128) {
+    window.alert("Please select a value between 8 and 128.");
+    generatePassword();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
